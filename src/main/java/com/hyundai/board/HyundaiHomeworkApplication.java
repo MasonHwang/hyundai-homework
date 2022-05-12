f0d5c998a7d488cc01bd55466a1a5b092cb746f1
myeongha <ghkd2676@naver.com>
2022-05-12 14:55:48 +0900

    create project

---
+++ src/main/java/com/hyundai/board/HyundaiHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.hyundai.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyundaiHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyundaiHomeworkApplication.class, args);
+	}
+
+}
